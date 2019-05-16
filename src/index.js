80b2b6127250d7ed066c3cd65c2e03a1db43dc26
Maya <zghbyslzf@163.com>
2019-05-16 10:19:26 +0800

    css添加前缀

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import './style.css'
 function component() {
   var element = document.createElement('div')
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.innerHTML = _.join(['Hello 哈哈', 'webpack 我'], ' ')
+  element.classList.add('hello')
 
   return element
 }
--- webpack.common.js
+++ webpack.common.js
@@ -37,7 +37,20 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
